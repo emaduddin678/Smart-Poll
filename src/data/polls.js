8868bddcfb940a00f12def0913cd4a5006b73850
Emad Uddin <emaduddin678@gmail.com>
2022-08-28 17:33:46 +0600

    first commit

---
+++ src/data/polls.js
@@ -0,0 +1,46 @@
+const polls = [
+  {
+    id: "45184",
+    title: "What is your Favorite Programming Language",
+    description:
+      "There are lot of popular Language available. Among them what is your favorite?",
+    option: [
+      { id: "ff14514", value: "C Programming", vote: 0 },
+      { id: "hgi1514", value: "Java", vote: 0 },
+      { id: "lk14514", value: "JavaScript", vote: 0 },
+      { id: "as14514", value: "Python", vote: 0 },
+    ],
+    created: new Date(),
+    totalVote: 0,
+    opinions: [],
+  },
+  {
+    id: "451sd84",
+    title: "Which Frontend FrameWork do you Prefer?",
+    description:
+      "JavaScript has a vast variety of frontend libraries and frameWorks. Each and every day we have a new one. Among those which frontend framework you like and prefer others?",
+    option: [
+      { id: "ff1451asdf4", value: "Angular", vote: 0 },
+      { id: "hgids1514", value: "React", vote: 0 },
+      { id: "lk14aec514", value: "Vue", vote: 0 }
+    ],
+    created: new Date(),
+    totalVote: 0,
+    opinions: [],
+  },
+  {
+    id: "451mon84",
+    title: "What is the best way to create android app?",
+    description:
+      "There are lot of popular Language available. Among them what is your favorite?",
+    option: [
+      { id: "ff14514", value: "C Programming", vote: 0 },
+      { id: "hgi1514", value: "Java", vote: 0 },
+      { id: "lk14514", value: "JavaScript", vote: 0 },
+      { id: "as14514", value: "Python", vote: 0 },
+    ],
+    created: new Date(),
+    totalVote: 0,
+    opinions: [],
+  },
+];
